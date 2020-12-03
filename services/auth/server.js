686f0d8e0279dbab6c8a9598455c22a0b1e1946a
Satyam Pawar <satyamp110@gmail.com>
2020-12-03 23:57:10 +0530

    Folder Restructure

--- services/auth/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const app = require('./app');
-
-const port = 3003;
-
-const server = http.createServer(app);
-
-server.listen(port,()=>console.log('Auth Service running on port '+port));
